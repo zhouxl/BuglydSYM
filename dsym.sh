b9271eb11b9bcd957a1ab8328619e75ef940ff59
小六 <zhouxl72@126.com>
2016-07-20 19:17:44 +0800

    打包并自动上传

--- dsym.sh
+++ dsym.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
+# author: 小六
+# email: zhouxl72@126.com
+# github: https://github.com/zhouxl/BuglydSYM
+eval `cat ./buglyConfig`
 
-path="$(find . -name '*.app.dSYM')" 
+path="$(find . -name '*.app.dSYM')"
 
-if [[ $path ]]; then
-	java -jar buglySymbolIos.jar -i $path
+if [[ $APPID ]] && [ $APPKEY ] && [ $PACKAGE ] && [ $path ]; then
+	echo 输入版本号
+	read version
+	if [[ $version ]]; then
+		java -jar buglySymbolIos.jar -i $path -u -id $APPID -key $APPKEY -package $PACKAGE -version	$version
+	else
+		echo -e "\033[31m必须输入版本号\033[0m"
+	fi
+
 else
-	echo -e "\033[31mDon\'t find the dSYM'file\033[0m"
+	if [[ $path ]]; then
+		java -jar buglySymbolIos.jar -i $path
+	else
+		echo -e "\033[31m没有在当前目录找到dSYM文件\033[0m"
+	fi
 fi
-
